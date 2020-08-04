9b41c33f9611c44eaf91cd9a503776f471891997
azagaya <azagaya.games@gmail.com>
2020-08-04 14:32:45 -0300

    Added missing imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,6 @@
+import sphinx_rtd_theme
+import sys
+import os
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
